69da8e0ffe2cf7cbdc7ee13ea93dd03592fa452b
FabioDrizZt <ing.fabioarg@gmail.com>
2026-02-19 11:11:47 -0300

    asd

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const { resolve } = require('path');
 
 const app = express();
 const port = 3010;
 
-app.use(express.static('static'));
+app.use(express.static('trailerflix'));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
